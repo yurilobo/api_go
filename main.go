ebba749b864182e0b0ed0d769eac8ad776433726
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-31 23:53:21 -0300

    Tentando usar algo parecido com metodos

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ type Car struct {
 	Model string
 }
 
+func (c Car) Andar() {
+	fmt.Println("O carro", c.Name, "está andando.")
+}
+
 func main() {
 	carro := Car{"Fusca", "VW"}
-	fmt.Println(carro.Name)
+	//fmt.Println(carro.Name)
 	carro.Model = "Fiat"
-	fmt.Println(carro.Model)
+	//fmt.Println(carro.Model)
+	carro.Andar()
 }
