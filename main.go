17813abdd95aa34ed48f5a49b1e983315bf71105
Yuri <yuri.anderson.lobo@gamail.com>
2024-01-31 23:43:23 -0300

    funcao soma

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var a int
-	a = 10
-	fmt.Println(a)
+	result, err := soma(1, 2)
+	fmt.Println(result, err)
+}
+func soma(a int, b int) (int, error) {
+	if a+b > 10 {
+		return 0, fmt.Errorf("soma maior que 10")
+	}
+	return a + b, nil
 }
