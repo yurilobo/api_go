fd867c918ae53d6e4f0ac0d48125466684eafff1
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-05 23:41:18 -0300

    Primeira api em go

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import "github.com/labstack/echo/v4"
 
 type Car struct {
 	Name  string
 	Price float64
 }
 
-func (c Car) Andar() {
-	fmt.Println("O carro", c.Name, "está andando.")
+var cars []Car
+
+func createCars() {
+	cars = append(cars, Car{Name: "Ferrari", Price: 250.000})
+	cars = append(cars, Car{Name: "Jaguar", Price: 150.000})
+	cars = append(cars, Car{Name: "Corola", Price: 120.000})
 }
 
 func main() {
-	carro := Car{"Fusca", "VW"}
-	//fmt.Println(carro.Name)
-	carro.Model = "Fiat"
-	//fmt.Println(carro.Model)
-	carro.Andar()
+	createCars()
+	e := echo.New()
+	e.GET("/cars", getCars)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getCars(c echo.Context) error {
+	return c.JSON(200, cars)
 }
